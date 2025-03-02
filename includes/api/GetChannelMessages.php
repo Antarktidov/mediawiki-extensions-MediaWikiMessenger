a57ce9908d5ca8d9bb3339953dbf08d72fc6acff
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2025-03-02 22:54:48 +0300

    finishing GetChannelMessages.php + fixing sql files

--- includes/api/GetChannelMessages.php
+++ includes/api/GetChannelMessages.php
@@ -58,49 +58,51 @@ class GetChannelMessages extends ApiBase {
             $this->getResult()->addValue( null, $this->getModuleName(), [
                 'channel' => $rows,
             ] );
-                
-            try {
-                //$dbProvider = MediaWikiServices::getInstance()->getConnectionProvider();
-                //$dbr = $dbProvider->getReplicaDatabase();
-    
-                $res = $dbr->newSelectQueryBuilder()
-                ->select( ['mw_messenger_message_id', 'mw_messenger_message_user_id',
-                'mw_messenger_message_channel_id', 'is_editing_restricted_to_chatmods',
-                'is_editing_restricted_to_chatadmins',
-                'is_deleted'] )
-                ->from('mw_messenger_message')
-                ->where('is_deleted = 0')
-                ->where("mw_messenger_message_channel_id = $channel_id")
+
+            $res = $dbr->newSelectQueryBuilder()
+            ->select( ['mw_messenger_message_id', 'mw_messenger_message_user_id',
+            'mw_messenger_message_channel_id', 'is_editing_restricted_to_chatmods',
+            'is_editing_restricted_to_chatadmins',
+            'is_deleted'] )
+            ->from('mw_messenger_message')
+            ->where('is_deleted = 0')
+            ->where("mw_messenger_message_channel_id = $channel_id")
+            ->fetchResultSet();
+
+            $messages = [];
+
+            foreach ( $res as $row ) {
+                $mw_messenger_message_user_id = $row->mw_messenger_message_user_id;
+
+                $userRes = $dbr->newSelectQueryBuilder()
+                ->select( ['user_name'] )
+                ->from('user')
+                ->where("user_id = $mw_messenger_message_user_id")
                 ->fetchResultSet();
-    
-                $rows = [];
-    
-                foreach ( $res as $row ) {
-                    array_push($rows, $row);
+
+                foreach ( $userRes as $userRow ) {
+                    $row->user_name = $userRow->user_name;
                 }
-    
-                /*if ((!$user->isAllowed('view_chatadmins_private_channels')) &&
-                    $rows->is_chatadmins_private ) {
-                    return;
+
+                $mw_messenger_message_revision_message_id = $row->mw_messenger_message_id;
+
+                $revisionRes = $dbr->newSelectQueryBuilder()
+                ->select( ['mw_messenger_message_revision_text', 'mw_messenger_message_revision_message_id'] )
+                ->from('mw_messenger_message_revision')
+                ->where("mw_messenger_message_revision_message_id = $mw_messenger_message_revision_message_id")
+                ->fetchResultSet();
+
+                foreach ( $revisionRes as $revisionRow ) {
+                    $row->mw_messenger_message_revision_text = $revisionRow->mw_messenger_message_revision_text;
+                    $row->mw_messenger_message_revision_message_id = $revisionRow->mw_messenger_message_revision_message_id;
                 }
-    
-                if ((!$user->isAllowed('view_chatmods_private_channels')) &&
-                    $rows->is_chatmods_private ) {
-                    return;
-                }*/
-    
-                $this->getResult()->addValue( null, $this->getModuleName(), [
-                    'messages' => $rows,
-                ] );
-    
-                
-    
-            } catch ( DBQueryError $e ) {
-                $this->getResult()->addValue( null, $this->getModuleName(), [
-                    'result' => 'error',
-                    'error' => $e->getMessage()
-                ] );
+
+                array_push($messages, $row);
             }
+
+            $this->getResult()->addValue( null, $this->getModuleName(), [
+                'messages' => $messages,
+            ]);
 
         } catch ( DBQueryError $e ) {
             $this->getResult()->addValue( null, $this->getModuleName(), [
@@ -109,22 +111,22 @@ class GetChannelMessages extends ApiBase {
             ] );
         }
     }
+
     /** @inheritDoc */
-	public function getAllowedParams() {
-		return [
-			'channel_id' => [
-				ApiBase::PARAM_REQUIRED => true,
-				ApiBase::PARAM_TYPE => 'integer'
+    public function getAllowedParams() {
+        return [
+            'channel_id' => [
+                ApiBase::PARAM_REQUIRED => true,
+                ApiBase::PARAM_TYPE => 'integer'
             ],
             'page' => [
-				ApiBase::PARAM_REQUIRED => false,
-				ApiBase::PARAM_TYPE => 'integer'
+                ApiBase::PARAM_REQUIRED => false,
+                ApiBase::PARAM_TYPE => 'integer'
             ],
             'is_last' => [
-				ApiBase::PARAM_REQUIRED => false,
-				ApiBase::PARAM_TYPE => 'boolean'
-                //default: true
+                ApiBase::PARAM_REQUIRED => false,
+                ApiBase::PARAM_TYPE => 'boolean'
             ]
-		];
-	}
+        ];
+    }
 }
--- sql/mw_messenger_message_revision.sql
+++ sql/mw_messenger_message_revision.sql
@@ -1,7 +1,8 @@
 CREATE TABLE /*_*/mw_messenger_message_revision (
   mw_messenger_message_revision_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  mw_messenger_message_revision_text LONGTEXT NOT NULL,
   mw_messenger_message_revision_user_id INT NOT NULL,
-  mw_messenger_message_channel_id INT NOT NULL,
+  mw_messenger_message_revision_message_id INT NOT NULL,
   is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
   PRIMARY KEY(
     mw_messenger_message_revision_id
