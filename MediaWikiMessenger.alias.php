7196383d58f9d0911591f4c0c5372df27683fccb
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2025-03-03 17:12:32 +0300

    added special page alias

---
+++ MediaWikiMessenger.alias.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Special page aliases for the MediaWikiMessenger extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English */
+$specialPageAliases['en'] = [
+	'Messenger' => [ 'Messenger' ]
+];
+
+/** Russian */
+$specialPageAliases['ru'] = [
+	'Messenger' => [ 'Мессенджер' ]
+];
